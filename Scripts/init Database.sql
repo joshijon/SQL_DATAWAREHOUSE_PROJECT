18c76c08e3bf20eb8f4353e62e12043a94bb9232
joshijon <joshijon1010@gmail.com>
2026-01-08 01:42:28 +0100

    Add files via upload

---
+++ Scripts/init Database.sql
@@ -0,0 +1,18 @@
+
+USE master;
+
+-- CREATING DATABASE 'DataWarehouse'
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- CREATE SCHEMAS
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
